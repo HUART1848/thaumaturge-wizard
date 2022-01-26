b2e674b243ef4c3847f3b6e73eff22da31a877d1
Hugo Huart <hugo.huart@heig-vd.ch>
2022-01-26 21:29:44 +0100

    Création du squelette des métriques

--- resources/index.php
+++ resources/index.php
@@ -11,7 +11,6 @@ $page->addValueToPage("<h1>Thaumaturge Wizard");
 $page->addValueToPage("<h2>Système de gestion de tournois Magic: The Gathering");
 
 $page->addValueToPage("Accès vers les fonctionnalités:");
-
 $page->addLinkListToPage("", $features);
 
 $page->getInstance()->render();
