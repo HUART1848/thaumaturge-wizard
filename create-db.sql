941356d1ba59186546fc47b4763764eb0c417cfd
Hugo Huart <hugo.huart@heig-vd.ch>
2022-01-27 09:23:10 +0100

    Add DB creation script

---
+++ create-db.sql
@@ -0,0 +1,203 @@
+
+
+/* ------------------------------------------------------------------ */ 
+/* DECLARATION                                                        */
+/* ------------------------------------------------------------------ */ 
+
+DROP TABLE IF EXISTS Adresse CASCADE;
+CREATE TABLE Adresse (
+	id SMALLSERIAL,
+	ville TEXT NOT NULL,
+	pays TEXT NOT NULL,
+	PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS Personne CASCADE;
+CREATE TABLE Personne (
+	id SMALLSERIAL,
+	nom TEXT NOT NULL,
+	prenom TEXT NOT NULL,
+	idAdresse SMALLINT NOT NULL,
+	PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS Juge CASCADE;
+CREATE TABLE Juge (
+	idPersonne SMALLINT,
+	experience INT NOT NULL,
+	PRIMARY KEY(idPersonne)
+);
+
+DROP TABLE IF EXISTS Membre CASCADE;
+CREATE TABLE Membre (
+	idPersonne SMALLINT,
+	numeroMembre INT NOT NULL,
+	PRIMARY KEY(idPersonne)
+);
+
+DROP TABLE IF EXISTS Duel CASCADE;
+CREATE TABLE Duel (
+	id SMALLSERIAL,
+	idManche SMALLINT,
+	idTournoi SMALLINT,
+	idJuge SMALLINT NOT NULL,
+	idJoueurUn SMALLINT NOT NULL,
+	idJoueurDeux SMALLINT NOT NULL,
+	idGagnant SMALLINT,
+	CONSTRAINT gagnant_check CHECK (idGagnant = NULL OR idGagnant = idJoueurUn OR idGagnant = idJoueurDeux),
+	CONSTRAINT joueurs_check CHECK (idJoueurUn <> idJoueurDeux),
+	PRIMARY KEY (id, idManche, idTournoi)
+);
+
+DROP TABLE IF EXISTS Manche CASCADE;
+CREATE TABLE Manche (
+	id SMALLSERIAL,
+	idTournoi SMALLINT,
+	dateHeureDebut TIMESTAMP NOT NULL,
+	duree INTERVAL NOT NULL,
+	PRIMARY KEY (id, idTournoi)
+);
+
+DROP TABLE IF EXISTS Tournoi CASCADE;
+CREATE TABLE Tournoi (
+	id SMALLSERIAL,
+	nom TEXT NOT NULL,
+	dateHeureDebut TIMESTAMP NOT NULL,
+	dateHeureFin TIMESTAMP NOT NULL CONSTRAINT time_check CHECK (dateHeureFin > dateHeureDebut), 
+	delaiAdmin TIME NOT NULL,
+	format TEXT NOT NULL,
+	echelleNbJoueur SMALLINT NOT NULL,
+	idAdresse SMALLINT NOT NULL,
+	PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS TournoiMembreParticipant CASCADE;
+CREATE TABLE TournoiMembreParticipant (
+	idMembre SMALLINT,
+	idTournoi SMALLINT,
+	nomDuDeck TEXT NOT NULL,
+	cardList TEXT NOT NULL,
+	PRIMARY KEY (idMembre, idTournoi)
+);
+
+DROP TABLE IF EXISTS TournoiMembreOrganisateur CASCADE;
+CREATE TABLE TournoiMembreOrganisateur (
+	idOrg SMALLINT,
+	idTournoi SMALLINT,
+	PRIMARY KEY (idOrg, idTournoi)
+);
+
+DROP TABLE IF EXISTS TournoiJuge CASCADE;
+CREATE TABLE TournoiJuge (
+	idJuge SMALLINT,
+	idTournoi SMALLINT,
+	PRIMARY KEY (idJuge, idTournoi)
+);
+
+/* ------------------------------------------------------------------ */ 
+/* CONSTRAINTS                                                        */
+/* ------------------------------------------------------------------ */ 
+
+ALTER TABLE Juge ADD CONSTRAINT idPersonne
+     FOREIGN KEY (idPersonne)
+     REFERENCES Personne (id)
+	 ON DELETE CASCADE
+     ON UPDATE CASCADE;
+
+ALTER TABLE Membre ADD CONSTRAINT FK_Membre_idPersonne
+     FOREIGN KEY (idPersonne)
+     REFERENCES Personne (id)
+     ON DELETE CASCADE
+     ON UPDATE CASCADE;
+
+ALTER TABLE Personne ADD CONSTRAINT FK_Personne_idAdresse
+	FOREIGN KEY (idAdresse)
+	REFERENCES Adresse (id)
+	ON DELETE SET NULL 
+	ON UPDATE CASCADE;
+	
+ALTER TABLE Tournoi ADD CONSTRAINT FK_Tournoi_idAdresse
+	FOREIGN KEY (idAdresse)
+	REFERENCES Adresse (id)
+	ON DELETE SET NULL 
+	ON UPDATE CASCADE;
+	
+ALTER TABLE Manche ADD CONSTRAINT FK_Manche_idTournoi
+	FOREIGN KEY (idTournoi)
+	REFERENCES Tournoi (id)
+	ON DELETE CASCADE
+	ON UPDATE CASCADE;
+	
+ALTER TABLE Duel ADD CONSTRAINT FK_Duel_idManche
+	FOREIGN KEY (idManche, idTournoi)
+	REFERENCES Manche (id, idTournoi)
+	ON DELETE CASCADE
+	ON UPDATE CASCADE;
+	
+ALTER TABLE Duel ADD CONSTRAINT FK_Duel_idJoueurUn
+	FOREIGN KEY (idJoueurUn)
+	REFERENCES Membre (idPersonne)
+	ON DELETE NO ACTION 
+	ON UPDATE CASCADE;
+	
+ALTER TABLE Duel ADD CONSTRAINT FK_Duel_idJoueurDeux
+	FOREIGN KEY (idJoueurDeux)
+	REFERENCES Membre (idPersonne)
+	ON DELETE NO ACTION 
+	ON UPDATE CASCADE;
+	
+ALTER TABLE Duel ADD CONSTRAINT FK_Duel_idGagnant
+	FOREIGN KEY (idGagnant)
+	REFERENCES Membre (idPersonne)
+	ON DELETE NO ACTION 
+	ON UPDATE CASCADE;
+
+ALTER TABLE Duel ADD CONSTRAINT FK_Duel_idJuge
+	FOREIGN KEY (idJuge)
+	REFERENCES Juge (idPersonne)
+	ON DELETE NO ACTION 
+	ON UPDATE CASCADE;
+
+ALTER TABLE tournoiJuge ADD CONSTRAINT FK_TournoiJuge_idJuge
+	FOREIGN KEY (idJuge)
+	REFERENCES Juge (idPersonne)
+	ON DELETE NO ACTION 
+	ON UPDATE CASCADE;
+
+ALTER TABLE tournoiJuge ADD CONSTRAINT FK_TournoiJuge_idTournoi
+	FOREIGN KEY (idTournoi)
+	REFERENCES Tournoi (id)
+	ON DELETE CASCADE 
+	ON UPDATE CASCADE;
+
+ALTER TABLE TournoiMembreOrganisateur ADD CONSTRAINT FK_TournoiJuge_idOtg
+	FOREIGN KEY (idOrg)
+	REFERENCES Membre (idPersonne)
+	ON DELETE NO ACTION 
+	ON UPDATE CASCADE;
+
+ALTER TABLE TournoiMembreOrganisateur ADD CONSTRAINT FK_TournoiJuge_idTournoi
+	FOREIGN KEY (idTournoi)
+	REFERENCES Tournoi (id)
+	ON DELETE CASCADE 
+	ON UPDATE CASCADE;
+
+ALTER TABLE TournoiMembreParticipant ADD CONSTRAINT FK_TournoiJuge_idMembre
+	FOREIGN KEY (idMembre)
+	REFERENCES Membre (idPersonne)
+	ON DELETE NO ACTION 
+	ON UPDATE CASCADE;
+
+ALTER TABLE TournoiMembreParticipant ADD CONSTRAINT FK_TournoiJuge_idTournoi
+	FOREIGN KEY (idTournoi)
+	REFERENCES Tournoi (id)
+	ON DELETE CASCADE 
+	ON UPDATE CASCADE;
+
+
+/*TODO lien tournoi DONE
+/*TODO EA à jour
+/*TODO duel à jour Done
+/*TODO clé étrangère bien nommée DONE
+/*TODO ON DELETE à jour (peut-être pas CASCADE) DONE
+/*TODO CHECK dates DONE*/
