551570ada2cecc84ace157a7d7565bb9bf42c5d1
Hugo Huart <hugo.huart@heig-vd.ch>
2022-01-27 15:19:33 +0100

    Update scripts (Add cities and tournaments création)

---
+++ init.sql
@@ -0,0 +1,79 @@
+/** ADRESSES **/
+/*1*/ INSERT INTO adresse (ville, pays) VALUES ('Yverdon-les-Bains', 'Suisse');
+/*2*/ INSERT INTO adresse (ville, pays) VALUES ('Lausanne', 'Suisse');
+/*3*/ INSERT INTO adresse (ville, pays) VALUES ('Bienne', 'Suisse');
+/*4*/ INSERT INTO adresse (ville, pays) VALUES ('Fribourg', 'Suisse');
+/*5*/ INSERT INTO adresse (ville, pays) VALUES ('Paris', 'France');
+/*6*/ INSERT INTO adresse (ville, pays) VALUES ('Kyiv', 'Ukraine');
+
+
+/** TOURNOIS **/
+/* 1*/ INSERT INTO tournoi (nom, dateheuredebut, dateheurefin, delaiadmin, format, echellenbjoueur, idadresse)
+	VALUES ('Impériale du Dragon',
+			'2021-09-11 06:00',
+			'2021-09-11 23:00',
+			'2021-09-12 00:00',
+			'Standard', 24, 1);
+		
+/* 2*/ INSERT INTO tournoi (nom, dateheuredebut, dateheurefin, delaiadmin, format, echellenbjoueur, idadresse)
+	VALUES ('Pourax magenta',
+			'2022-01-20 12:00',
+			'2022-01-21 04:00',
+			'2022-01-30 00:00',
+			'EDH', 12, 1);
+		
+/* 3*/ INSERT INTO tournoi (nom, dateheuredebut, dateheurefin, delaiadmin, format, echellenbjoueur, idadresse)
+	VALUES ('Biennale biennoise des jeux de cartes',
+			'2022-06-01 10:00',
+			'2022-06-08 12:00',
+			'2022-06-10 00:00',
+			'Standard', 64, 3);
+		
+/* 4*/ INSERT INTO tournoi (nom, dateheuredebut, dateheurefin, delaiadmin, format, echellenbjoueur, idadresse)
+	VALUES ('Tournoi Larigot-Parmalat',
+			'2021-12-06 10:00',
+			'2021-12-07 13:00',
+			'2021-12-10 00:00',
+			'Vintage', 24, 4);
+		
+/* 5*/ INSERT INTO tournoi (nom, dateheuredebut, dateheurefin, delaiadmin, format, echellenbjoueur, idadresse)
+	VALUES ('Xtreme contest Romande Énergie',
+			'2022-03-30 12:00',
+			'2022-03-31 00:00',
+			'2022-04-14 00:00',
+			'Standard', 128, 2);
+		
+/* 6*/ INSERT INTO tournoi (nom, dateheuredebut, dateheurefin, delaiadmin, format, echellenbjoueur, idadresse)
+	VALUES ('Tournoi de la guilde Vaudoise',
+			'2022-03-30 12:00',
+			'2022-04-05 12:00',
+			'2022-04-20 00:00',
+			'Modern', 64, 2);
+		
+/* 7*/ INSERT INTO tournoi (nom, dateheuredebut, dateheurefin, delaiadmin, format, echellenbjoueur, idadresse)
+	VALUES ('Francilienne des jeux de carte',
+			'2022-02-08 08:00',
+			'2022-02-09 10:00',
+			'2021-02-16 00:00',
+			'Modern', 64, 5);
+		
+/* 8*/ INSERT INTO tournoi (nom, dateheuredebut, dateheurefin, delaiadmin, format, echellenbjoueur, idadresse)
+	VALUES ('Japan Expo',
+			'2022-04-01 10:00',
+			'2022-04-01 22:00',
+			'2022-04-08 00:00',
+			'EDH', 2, 5);
+		
+/* 9*/ INSERT INTO tournoi (nom, dateheuredebut, dateheurefin, delaiadmin, format, echellenbjoueur, idadresse)
+	VALUES ('Mizhnarodnyy turnir Magic',
+			'2022-02-28 06:30',
+			'2022-03-01 07:00',
+			'2022-03-08 00:00',
+			'Vintage', 2, 6);
+		
+/*10*/ INSERT INTO tournoi (nom, dateheuredebut, dateheurefin, delaiadmin, format, echellenbjoueur, idadresse)
+	VALUES ('Tournoi du cercle d''amitié de Merlin l''enchanteur',
+			'2022-07-03 10:00',
+			'2022-07-03 19:00',
+			'2022-07-17 00:00',
+			'Standard', 16, 1);
