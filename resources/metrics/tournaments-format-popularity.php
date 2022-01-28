9416d34c4d34d755d0693e06cbac62ac75615282
Hugo Huart <hugo.huart@heig-vd.ch>
2022-01-28 23:19:50 +0100

    tournaments metrics done

--- resources/metrics/tournaments-format-popularity.php
+++ resources/metrics/tournaments-format-popularity.php
@@ -8,6 +8,37 @@ $page->setHttpCode(HttpCodes::REQUEST_VALID);
 $page->addValueToPage("<h1>Formats de tournois les plus populaires</h1>");
 $page->addLinkToPage("Retour aux métriques", "metrics.php", "../");
 
+$pageContent = getTournamentsFormatPopularity();
+
+foreach ($pageContent as $row) {
+    $page->addValueToPage($row["format"] . " : " . $row["occurence"] );
+}
+
 $page->getInstance()->render();
+
+function getTournamentsFormatPopularity() {
+    try {
+        $dbh = new PDO('pgsql:host=localhost;port=5432;dbname=postgres;', 'root', 'root', [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, //set PDO to throw exceptions on error
+            PDO::ATTR_ORACLE_NULLS => PDO::NULL_NATURAL, //NULL values are returned as PHP null's
+        ]);
+        $dataList = [];
+        $result = $dbh->exec('SET search_path TO wizard');
+        $stmt = $dbh->query('
+        SELECT format, COUNT(id) as occurence
+        FROM tournoi
+        GROUP BY format
+        ');
+        while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+            $dataList[] = [
+                'format' => $row['format'],
+                'occurence' => $row['occurence']
+            ];
+        }
+        return $dataList;
+    } catch(PDOException $e) {
+        die('Unable to get player list: ' . $e->getMessage());
+    }
+}
 
 ?>
--- resources/metrics/tournaments-popularity.php
+++ resources/metrics/tournaments-popularity.php
@@ -8,6 +8,39 @@ $page->setHttpCode(HttpCodes::REQUEST_VALID);
 $page->addValueToPage("<h1>Tournois les plus populaires</h1>");
 $page->addLinkToPage("Retour aux métriques", "metrics.php", "../");
 
+$pageContent = getTournamentsPopularity();
+
+foreach ($pageContent as $row) {
+    $page->addValueToPage($row["nbParticipants"] . " participants: " . $row["nom"] );
+}
+
 $page->getInstance()->render();
+
+function getTournamentsPopularity() {
+    try {
+        $dbh = new PDO('pgsql:host=localhost;port=5432;dbname=postgres;', 'root', 'root', [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, //set PDO to throw exceptions on error
+            PDO::ATTR_ORACLE_NULLS => PDO::NULL_NATURAL, //NULL values are returned as PHP null's
+        ]);
+        $dataList = [];
+        $result = $dbh->exec('SET search_path TO wizard');
+        $stmt = $dbh->query('
+        SELECT tournoi.nom, COUNT(tournoimembreparticipant.idtournoi) as nbParticipants
+        FROM tournoi
+        INNER JOIN tournoimembreparticipant on tournoi.id = tournoimembreparticipant.idtournoi
+        GROUP BY tournoi.nom
+        ORDER BY nbParticipants DESC
+        ');
+        while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+            $dataList[] = [
+                'nom' => $row['nom'],
+                'nbParticipants' => $row['nbparticipants']
+            ];
+        }
+        return $dataList;
+    } catch(PDOException $e) {
+        die('Unable to get player list: ' . $e->getMessage());
+    }
+}
 
 ?>
