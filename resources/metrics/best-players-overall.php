fff454799ac955ada6ecf3e136a68636ea400664
Joan MAILLARD <joan@nerdgautica.net>
2022-01-28 23:50:32 +0100

    Add relevant SQL queries.

--- resources/metrics/best-players-overall.php
+++ resources/metrics/best-players-overall.php
@@ -6,8 +6,48 @@ $page = new PageFactory();
 $page->setHttpCode(HttpCodes::REQUEST_VALID);
 
 $page->addValueToPage("<h1>Meilleurs joueurs tout formats confondus</h1>");
+
+/* TODO call function and map data to display */
+
 $page->addLinkToPage("Retour aux métriques", "metrics.php", "../");
 
 $page->getInstance()->render();
+
+function getPlayersList() {
+    try {
+        $dbh = new PDO('pgsql:host=localhost;port=5432;dbname=postgres;', 'root', 'root', [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, //set PDO to throw exceptions on error
+            PDO::ATTR_ORACLE_NULLS => PDO::NULL_NATURAL, //NULL values are returned as PHP null's
+        ]);
+        $dataList = [];
+        $result = $dbh->exec('SET search_path TO wizard');
+        $stmt = $dbh->query('
+            SELECT
+                Personne.nom,
+                Personne.prenom,
+                Membre.numeroMembre,
+                SUM(CASE WHEN Duel.idGagnant = Membre.idPersonne THEN 2
+                        WHEN Duel.idGagnant IS NULL THEN 0
+                        ELSE -2
+                        END) AS score
+            FROM Membre
+            INNER JOIN Personne
+                ON Personne.id = Membre.idPersonne
+            INNER JOIN Duel
+                ON Duel.idJoueurUn = Membre.idPersonne
+                    OR Duel.idJoueurDeux = Membre.idPersonne
+            GROUP BY (Personne.nom, Personne.prenom, Membre.numeroMembre)
+            ORDER BY score DESC;
+            ');
+        while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+            $dataList[] = [
+                /* TODO data mapping */
+            ];
+        }
+        return $dataList;
+    } catch(PDOException $e) {
+        die('Unable to get player list: ' . $e->getMessage());
+    }
+}
 
 ?>
